d5c79b8c726f48e687e0d18c6760c7557f78508c
SaiSarriyans <75849154+SaiSarriyans@users.noreply.github.com>
2022-06-21 14:06:08 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,51 @@
+cocossd="";
+img = "";
+img_b = "";
+/*img_h = "";*/
+function tb(){
+    window.location="tb.html";
+}
+function tt(){
+    window.location="tt.html";
+}
+function th(){
+    window.location="th.html";
+}
+function preload(){
+img_b = loadImage("Bottle.jpeg");
+/*img_h = loadImage("Head_phone.jpeg");*/
+}
+function setup() {
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    object_detector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "status: detecting objects";
+
+}
+/*function draw(){
+    image(img_b, 0, 0, 640, 420);
+    if (status != "") {
+        for (i = 0; i < objects.length; i++) {
+            document.getElementById("status").innerHTML = "status : Object Detected";
+            fill("#e8250c");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x, objects[i].y);
+            noFill();
+            stroke("#e8250c");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+}
+function modelLoaded() {
+    console.log("modelLoaded");
+    status = true;
+    object_detector.detect(img, gotResult);
+}
+
+function gotResult(error, Results) {
+    if (error) {
+        console.log(error);
+    }
+    console.log(Results);
+    objects = Results
+}*/
